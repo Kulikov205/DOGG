ef713b60411d2b13a4bb87d6b6a888ac0040fa95
dozopravka <dozopravka@example.com>
2023-04-10 18:46:53 +0300

    ib

--- app/src/main/java/com/example/ggd/MainActivity.java
+++ app/src/main/java/com/example/ggd/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.ggd;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Запуск dozo
-        Button button = findViewById(R.id.dozo);
+        ImageButton button = findViewById(R.id.dozo);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //Запуск kairos
-        Button button1 = findViewById(R.id.kairos);
+        ImageButton button1 = findViewById(R.id.kairos);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //Запуск vaga
-        Button button2 = findViewById(R.id.vaga);
+        ImageButton button2 = findViewById(R.id.vaga);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,10 +55,11 @@ public class MainActivity extends AppCompatActivity {
                 intent.setComponent(new ComponentName("com.example.vaga", "com.example.vaga.MainActivity"));
                 startActivity(intent);
             }
+
         });
 
         //Запуск pickle
-        Button button3 = findViewById(R.id.pickle);
+        ImageButton button3 = findViewById(R.id.pickle);
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -68,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //Запуск olman
-        Button button4 = findViewById(R.id.olman);
+        ImageButton button4 = findViewById(R.id.olman);
         button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
